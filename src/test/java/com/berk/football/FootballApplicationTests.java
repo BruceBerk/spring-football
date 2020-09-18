42ff83ad32fbf09bccf4bc2d1f0c29e7aa5b9eff
Bruce Berkowicz <berknj@Bruces-MacBook-Pro.local>
2020-09-18 10:49:20 -0400

    Initial commit

---
+++ src/test/java/com/berk/football/FootballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berk.football;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
