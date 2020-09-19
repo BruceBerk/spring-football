d8023393776e075313034aa823f44bb31190e3fa
Bruce Berkowicz <bruceberk@me.com>
2020-09-19 15:13:18 -0400

    Dice Roll unit tests start

---
+++ src/test/java/com/berk/football/GameControllerDiceTests.java
@@ -0,0 +1,12 @@
+package com.berk.football;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GameControllerDiceTests {
+    @Test
+    void fourDiceAreReturned() {
+
+    }
+}
