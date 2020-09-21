a80d5e6902a997425f1ba1ccc8543d3fae78b3c3
Bruce Berkowicz <bruceberk@me.com>
2020-09-21 17:07:32 -0400

    Continuing dice roll service tests

---
+++ src/test/java/com/berk/football/GameControllerTests.java
@@ -0,0 +1,43 @@
+package com.berk.football;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = GameController.class)
+@WebAppConfiguration
+public class GameControllerTests {
+    protected MockMvc mvc;
+
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setUp() {
+        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void getDiceRollTest() throws Exception {
+        String uri = "/game/roll";
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+                .accept(MediaType.ALL)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+    }
+}
