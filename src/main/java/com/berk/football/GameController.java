044b0baff73499b017e34f31ea539c7b7d514a76
Bruce Berkowicz <bruceberk@me.com>
2020-09-22 15:22:06 -0400

    Dice roll controller works, but test fails with NPE

--- src/main/java/com/berk/football/GameController.java
+++ src/main/java/com/berk/football/GameController.java
@@ -2,41 +2,50 @@ package com.berk.football;
 
 /* A REST controller for the dice rolling */
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 @RestController
-@RequestMapping("/game")
+@RequestMapping()
 public class GameController {
     private final Random randomGenerator = new Random();   // check if there should be a seed here
 
+    private static final Map<String, String> diceMap = new HashMap<>();
+
     @GetMapping("/roll")
-    public List<String> roll() {
-        List<String> dice = new ArrayList<>();
+    public ResponseEntity<Object> roll() {
+        //List<String> dice = new ArrayList<>();
+
         int whiteDie = randomGenerator.nextInt(6) + 1;
-        dice.add(Integer.toString(whiteDie));
+        //dice.add(Integer.toString(whiteDie));
+        diceMap.put("white", Integer.toString(whiteDie));
 
         int redDie1 = randomGenerator.nextInt(6) + 1;
-        dice.add(Integer.toString(redDie1));
+        //dice.add(Integer.toString(redDie1));
+        diceMap.put("red1", Integer.toString(redDie1));
 
         int redDie2 = randomGenerator.nextInt(6) + 1;
-        dice.add(Integer.toString(redDie2));
+        //dice.add(Integer.toString(redDie2));
+        diceMap.put("red2", Integer.toString(redDie2));
 
         // black die: 3 sides are blank, 2 sides have "D" and 1 side has "X"
         int blackDie = randomGenerator.nextInt(6) + 1;
         if (blackDie < 4) {
-            dice.add("_");
+            //dice.add("_");
+            diceMap.put("black", "_");
         } else if (blackDie < 6) {
-            dice.add("D");
+            //dice.add("D");
+            diceMap.put("black", "D");
         } else {
-            dice.add("X");
+            //dice.add("X");
+            diceMap.put("black", "X");
         }
 
-        return(dice);
+        return new ResponseEntity<>(diceMap, HttpStatus.OK);
     }
 }
--- src/test/java/com/berk/football/GameControllerTests.java
+++ src/test/java/com/berk/football/GameControllerTests.java
@@ -1,41 +1,33 @@
 package com.berk.football;
 
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = GameController.class)
 @WebAppConfiguration
-public class GameControllerTests {
-    protected MockMvc mvc;
-
-    @Autowired
-    WebApplicationContext webApplicationContext;
-
+public class GameControllerTests extends AbstractTest {
+    @Override
     @Before
     public void setUp() {
-        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        super.setUp();
     }
 
     @Test
-    public void getDiceRollTest() throws Exception {
-        String uri = "/game/roll";
+    public void rollTest() throws Exception {
+        String uri = "/roll";
 
         MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
-                .accept(MediaType.ALL)).andReturn();
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
 
         int status = mvcResult.getResponse().getStatus();
         assertEquals(200, status);
